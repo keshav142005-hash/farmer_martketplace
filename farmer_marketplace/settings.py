a34311dbd6cc8a8e36ab2c504ce253be7573d621
Keshav <keshav14.2005@gmail.com>
2026-06-18 14:43:11 +0530

    Prepare project for Render deployment

--- farmer_marketplace/settings.py
+++ farmer_marketplace/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['farmer-marketplace.onrender.com']
+ALLOWED_HOSTS =  [
+    '127.0.0.1',
+    'localhost',
+    'farmer-marketplace.onrender.com',
+]
 
 
 # Application definition
