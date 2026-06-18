b3fa624af731af0cd258598d7170a30d1750ad5d
Keshav <keshav14.2005@gmail.com>
2026-06-18 14:55:52 +0530

    Fix DisallowedHost

--- farmer_marketplace/settings.py
+++ farmer_marketplace/settings.py
@@ -26,10 +26,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS =  [
+ALLOWED_HOSTS = [
+    'farmer-marketplace.onrender.com',
+    'farmer-marketplace-1.onrender.com',
     '127.0.0.1',
     'localhost',
-    'farmer-marketplace.onrender.com',
 ]
 
 
